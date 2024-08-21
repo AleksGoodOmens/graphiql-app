8149fb0354037c9680ddd8faa99929c13c7b8f16
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-08-21 09:37:06 +0100

    fix: eslint, prettier, lintstaged test3

--- codeade/src/app/page.tsx
+++ codeade/src/app/page.tsx
@@ -1,8 +1,8 @@
 import { Intro } from '@/components'
-const r = 2
+const r = 3
 
 export default function Home() {
-	const a = (b) => b + 2
+	const a = (b) => b + 3
 	return (
 		<main>
 			<h1>home page</h1>
