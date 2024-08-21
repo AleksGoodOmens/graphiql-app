7ebb42b40327b5b835b66a0d0d6cc4d7639f820a
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-08-21 09:56:55 +0100

    test: husky2

--- codeade/src/app/page.tsx
+++ codeade/src/app/page.tsx
@@ -1,8 +1,8 @@
 import { Intro } from '@/components'
-const r = 7
+const r = 8
 
 export default function Home() {
-	const a = (b) => b + 7
+	const a = (b) => b + 8
 	return (
 		<main>
 			<h1>home page</h1>
