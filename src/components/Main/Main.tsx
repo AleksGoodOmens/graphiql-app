7561363c41f67fd7066f18bca1035575fa77da31
Darya Pchelintseva <raelin@mail.ru>
2024-09-07 23:36:42 +0300

    refactor: refactor code

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -14,61 +14,57 @@ export default function Main({ children }: { children: ReactNode }) {
 	const [user, loading] = useAuthState(auth)
 	const router = useRouter()
 
-	if (loading) {
-		return <Loading />
-	}
-
-	return (
-		<>
-			<main className={styles.main}>
-				<h1>CodeADE API Explorer</h1>
-				{user ? (
-					<div className={styles['main-wrapper']}>
-						<h2>Welcome Back, {user?.email}!</h2>
-						<div className={styles['main-links']}>
-							<Button
-								onClick={() => router.push('/rest_client')}
-								variant='contained'
-								color='success'
-							>
-								REST Client
-							</Button>
-							<Button
-								variant='contained'
-								color='success'
-							>
-								GraphiQL Client
-							</Button>
-							<Button
-								variant='contained'
-								color='success'
-							>
-								History
-							</Button>
-						</div>
-					</div>
-				) : (
-					<div className={styles['main-buttons']}>
+	return loading ? (
+		<Loading />
+	) : (
+		<main className={styles.main}>
+			<h1>CodeADE API Explorer</h1>
+			{user ? (
+				<div className={styles['main-wrapper']}>
+					<h2>Welcome Back, {user?.email}!</h2>
+					<div className={styles['main-links']}>
 						<Button
+							onClick={() => router.push('/rest_client')}
 							variant='contained'
-							startIcon={<Login />}
-							onClick={() => router.push('/signin')}
-							color='primary'
+							color='success'
 						>
-							Sign In
+							REST Client
 						</Button>
 						<Button
 							variant='contained'
-							startIcon={<AccountCircle />}
-							onClick={() => router.push('/signup')}
-							color='error'
+							color='success'
 						>
-							Sign Up
+							GraphiQL Client
+						</Button>
+						<Button
+							variant='contained'
+							color='success'
+						>
+							History
 						</Button>
 					</div>
-				)}
-				<div>{children}</div>
-			</main>
-		</>
+				</div>
+			) : (
+				<div className={styles['main-buttons']}>
+					<Button
+						variant='contained'
+						startIcon={<Login />}
+						onClick={() => router.push('/signin')}
+						color='primary'
+					>
+						Sign In
+					</Button>
+					<Button
+						variant='contained'
+						startIcon={<AccountCircle />}
+						onClick={() => router.push('/signup')}
+						color='error'
+					>
+						Sign Up
+					</Button>
+				</div>
+			)}
+			<div>{children}</div>
+		</main>
 	)
 }
